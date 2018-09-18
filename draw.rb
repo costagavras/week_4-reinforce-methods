c471a513b5b40c0432b4fd2bea056c1b2fbfbc94
costagavras <cgavras@yahoo.ca>
2018-09-18 12:32:41 -0400

    final

---
+++ draw.rb
@@ -0,0 +1,33 @@
+# Given the following Ruby method:
+
+# Pass in the appropriate argument when calling the method in order to generate the following output:
+#
+# ****
+# ****
+# ****
+# ****
+
+options = { :rows => 4, :cols => 4, :char => "*" }
+
+# Now pass in a different argument in order to generate the following output:
+# 000000000
+# 000000000
+# 000000000
+
+options = { :rows => 3, :cols => 9, :char => "0" }
+
+def draw_shape(options)
+  shape = ""
+
+  options[:rows].times do |r|
+    options[:cols].times do |c|
+      shape += options[:char]
+    end
+    shape += "\n"
+  end
+
+  return shape
+
+end
+
+puts draw_shape(options)
